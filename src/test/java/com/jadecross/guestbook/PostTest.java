51a5e8ea1898efa21c8868586553035632a9fc1e
devops <devops@jadecross.com>
2023-05-11 01:38:35 +0000

    [ADD] TestCase - PostTest.java

--- src/test/java/com/jadecross/guestbook/PostTest.java
+++ src/test/java/com/jadecross/guestbook/PostTest.java
@@ -7,6 +7,6 @@ class PostTest {
 	void testSetName() {
 		Post post = new Post("TESTER", "2022-04-12 01:01:48", "Happy Wedding");
 		post.setName("DEVOPS");
-		asserEquals("DEVOPS", post.getName());
+		assertEquals("DEVOPS", post.getName());
 	}	
 }	
